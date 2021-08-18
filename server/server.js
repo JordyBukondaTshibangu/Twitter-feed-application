8a649eeeda667c61627fabd2f2d53a7a6d7e9fef
Jordy Tshibangu <jordytshibss@gmail.com>
2021-08-18 07:08:06 +0200

    features : creating the twitter feed app server

--- server/server.js
+++ server/server.js
@@ -8,18 +8,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
 });
 
-// read files ( in the user file)
-
 readUsers;
-// insert files in data ()
-// format data
-// dispay formatted data
-// call a route to display data in the frontend
-
-
-
-// handle frontend request
-
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
